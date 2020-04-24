8d8b86a9fbdc017940ac105e107695337ba299b3
Darius Mandres <darius@cosdensolutions.io>
2020-04-24 11:43:45 +0200

    Part 2: Creating re-usable components

--- src/containers/homepage/index.jsx
+++ src/containers/homepage/index.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
+// Internal dependencies
+//
+import HeadlineCard from "../../components/headline-card";
+import InfoCard from "../../components/info-card";
+import Button from "../../components/button";
+
 const Homepage = () => {
-  return <div>This is the homepage</div>;
+  return (
+    <div className="homepage">
+      <div className="homepage__headline">
+        <HeadlineCard>
+          <h1>Trivia Game!</h1>
+        </HeadlineCard>
+      </div>
+      <div className="homepage__instructions">
+        <InfoCard>
+          <p>Instructions will go here</p>
+        </InfoCard>
+      </div>
+      <div className="homepage__tagline">
+        <HeadlineCard>
+          <h2>Can you score 100%?</h2>
+        </HeadlineCard>
+      </div>
+      <div className="homepage__button">
+        <Button text="Start Game" />
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
